ea730cb0f57853326708311b37a55682430bc008
Michele Comparini <comparini.michele@gmail.com>
2025-04-08 20:13:02 +0200

    Add first classes definition with first method definitions. Added test for getNumberOfRows and getNumberOfColumns.

---
+++ src/Spreadsheet.cpp
@@ -0,0 +1,13 @@
+#include "Spreadsheet.h"
+
+Spreadsheet::Spreadsheet(std::size_t numberOfColumns, std::size_t numberOfRows) {
+    //TODO: implementation
+}
+
+std::size_t Spreadsheet::getNumberOfColumns() const {
+    return 0; //TODO: implementation
+}
+
+std::size_t Spreadsheet::getNumberOfRows() const {
+    return 0; //TODO: implementation
+}
