b361ade9fb8bc20457ddfe38244ae649a42937c4
Michele Comparini <comparini.michele@gmail.com>
2025-04-11 17:02:17 +0200

    Added definition and tests for SpreadsheetCellCoordinates class

---
+++ src/SpreadsheetCellCoordinates.h
@@ -0,0 +1,16 @@
+#ifndef SPREADSHEETCELLCOORDINATES_H
+#define SPREADSHEETCELLCOORDINATES_H
+
+#include <string>
+
+class SpreadsheetCellCoordinates {
+    public:
+        SpreadsheetCellCoordinates(const std::string& columnIndex, std::size_t rowIndex);
+        SpreadsheetCellCoordinates(std::size_t columnIndex, std::size_t rowIndex);
+
+        std::size_t getRowIndex() const;
+        std::size_t getColumnIndexAsInteger() const;
+        std::string getColumnIndexAsString() const;
+};
+
+#endif //SPREADSHEETCELLCOORDINATES_H
