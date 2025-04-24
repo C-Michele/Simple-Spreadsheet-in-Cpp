a78fd47dc814fa8a0bd47a8800f563bdb98ece5c
Michele Comparini <141649328+C-Michele@users.noreply.github.com>
2025-04-24 16:40:38 +0200

    Implemented the constructor of Spreadsheet class. Implemented the Spreadsheet::getNumberOfColumns() method. Implemented the Spreadsheet::getNumberOfRaws() method. Implemented the Spreadsheet::emptyCellAt(const SpreadsheetCellCoordinates& coordinates) method. Added the private method 'void Spreadsheet::outOfRangeCheck(const SpreadsheetCellCoordinates&) const'. Added the private method 'void Spreadsheet::outOfRangeCheck(const std::set<SpreadsheetCellCoordinates>&) const'. Added the private method 'SpreadsheetCell* Spreadsheet::getCellAt(const SpreadsheetCellCoordinates& coordinates) const'.

--- src/Spreadsheet.cpp
+++ src/Spreadsheet.cpp
@@ -1,36 +1,79 @@
-#include "Spreadsheet.h"
+#include <stdexcept>
 
-Spreadsheet::Spreadsheet(std::size_t numberOfColumns, std::size_t numberOfRows) {
-    //TODO: implementation
+#include "Spreadsheet.h"
+#include "SpreadsheetEmptyCell.h"
+
+Spreadsheet::Spreadsheet(std::size_t const numberOfColumns, std::size_t const numberOfRows) : numberOfColumns(numberOfColumns) {
+    if ( numberOfRows != 0 ) {
+        cells.reserve(numberOfRows);
+        while ( cells.size() != numberOfRows ) {
+            cells.emplace_back();
+            if (numberOfColumns != 0) {
+                auto& rowToPopulate = cells.back();
+                try {
+                    rowToPopulate.reserve(numberOfColumns);
+                } catch (...) {
+                    cells.clear();
+                    this->numberOfColumns = 0;
+                    throw;
+                }
+                while ( rowToPopulate.size() != numberOfColumns ) {
+                    rowToPopulate.emplace_back( new SpreadsheetEmptyCell() );
+                }
+            }
+        }
+    }
 }
 
 std::size_t Spreadsheet::getNumberOfColumns() const {
-    return 0; //TODO: implementation
+    return numberOfColumns;
 }
 
 std::size_t Spreadsheet::getNumberOfRows() const {
-    return 0; //TODO: implementation
+    return cells.size();
 }
 
 bool Spreadsheet::emptyCellAt(const SpreadsheetCellCoordinates& coordinates) const {
-    return true; //TODO: implementation
+    return getCellAt(coordinates)->isEmpty();
 }
 
 void Spreadsheet::deleteCellContentAt(const SpreadsheetCellCoordinates& coordinates) {
-    //TODO: implementation
+    outOfRangeCheck(coordinates);
+    //TODO: Continue implementation from here
 }
 
 void Spreadsheet::setValueAt(const SpreadsheetCellCoordinates& coordinates, double value) {
-    //TODO: implementation
+    outOfRangeCheck(coordinates);
+    //TODO: Continue implementation from here
 }
 
 void Spreadsheet::setValueAt(const SpreadsheetCellCoordinates& coordinates, const std::string& value) {
-    //TODO: implementation
+    outOfRangeCheck(coordinates);
+    //TODO: Continue implementation from here
 }
 
 void Spreadsheet::setFunctionAt(
     const SpreadsheetCellCoordinates& coordinates,
     Function function,
     const std::set<SpreadsheetCellCoordinates>& functionArguments ) {
-    //TODO: implementation
+    outOfRangeCheck(coordinates);
+    outOfRangeCheck(functionArguments);
+    //TODO: Continue implementation from here
+}
+
+SpreadsheetCell* Spreadsheet::getCellAt(const SpreadsheetCellCoordinates& coordinates) const {
+    outOfRangeCheck(coordinates);
+    return ( ( cells[coordinates.getRowIndex()-1] )[coordinates.getColumnIndexAsInteger()-1] ).get();
+}
+
+void Spreadsheet::outOfRangeCheck(const SpreadsheetCellCoordinates& coordinates) const {
+    if ( coordinates.getRowIndex() > getNumberOfRows() || coordinates.getColumnIndexAsInteger() > getNumberOfColumns() ) {
+        throw std::out_of_range(" "); //TODO: insert an error message
+    }
+}
+
+void Spreadsheet::outOfRangeCheck(const std::set<SpreadsheetCellCoordinates>& coordinatesSet) const {
+    for (auto itr = coordinatesSet.cbegin(); itr != coordinatesSet.cend(); ++itr) {
+        outOfRangeCheck(*itr);
+    }
 }
