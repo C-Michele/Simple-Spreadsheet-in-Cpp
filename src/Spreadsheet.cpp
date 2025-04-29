e5edf3808ece23673df4a162ab1251d8ea713557
Michele Comparini <comparini.michele@gmail.com>
2025-04-29 16:39:32 +0200

    Added Spreadsheet::setFunctionAt implementation

--- src/Spreadsheet.cpp
+++ src/Spreadsheet.cpp
@@ -6,6 +6,7 @@
 #include "SpreadsheetNumericCell.h"
 #include "SpreadsheetRawNumericCell.h"
 #include "SpreadsheetRawTextualCell.h"
+#include "SpreadsheetSumCell.h"
 
 Spreadsheet::Spreadsheet(std::size_t const numberOfColumns, std::size_t const numberOfRows) : numberOfColumns(numberOfColumns) {
     if ( numberOfRows != 0 ) {
@@ -161,7 +162,7 @@ void Spreadsheet::setValueAt(const SpreadsheetCellCoordinates& coordinates, cons
 
 void Spreadsheet::setFunctionAt(
     const SpreadsheetCellCoordinates& coordinates,
-    Function function,
+    Function const function,
     const std::set<SpreadsheetCellCoordinates>& functionArguments ) {
     outOfRangeCheck(coordinates);
     outOfRangeCheck(functionArguments);
@@ -170,7 +171,98 @@ void Spreadsheet::setFunctionAt(
         //TODO: Implement a system to detect more complex circular references
         throw std::invalid_argument(" "); //TODO: add error message
     }
-    //TODO: Continue implementation from here
+
+    if ( function != Spreadsheet::Function::sum ) {  // For now only the sum function is supported
+        throw std::runtime_error("Not implemented function");
+    }
+
+    auto& rowContainingTheOldCell = cells[coordinates.getRowIndex()-1];
+    auto itrPointingTheOldCell = rowContainingTheOldCell.begin();
+    for (std::size_t j = 1; j != coordinates.getColumnIndexAsInteger(); ++j) {
+        ++itrPointingTheOldCell;
+    }
+
+    const auto oldCellObservers = (*itrPointingTheOldCell)->getObservers();
+    const bool oldCellIsEmpty = (*itrPointingTheOldCell)->isEmpty();
+    const std::string oldCellAsText = (*itrPointingTheOldCell)->getAsText();
+    const bool oldCellIsNumeric = isNumericCell( (*itrPointingTheOldCell).get() );
+    const double oldCellAsNumericValue =
+        oldCellIsNumeric ?
+        dynamic_cast<SpreadsheetNumericCell*>( (*itrPointingTheOldCell).get() )->getAsNumericValue() :
+        0.0 ;
+    const bool oldCellIsFunction = isFunctionCell( (*itrPointingTheOldCell).get() );
+    const auto oldCellArguments =
+        oldCellIsFunction ?
+        dynamic_cast<SpreadsheetFunctionCell*>( (*itrPointingTheOldCell).get() )->getArguments() :
+        std::set<SpreadsheetCell*>() ;
+    const Spreadsheet::Function oldCellFunction =
+        oldCellIsFunction ?
+        dynamic_cast<SpreadsheetFunctionCell*>( (*itrPointingTheOldCell).get() )->getFunction() :
+        Spreadsheet::Function::sum ;
+    const std::set<SpreadsheetCell*> newFunctionCellArguments = getCellsAt(functionArguments);
+
+    const auto itrFollowingTheDeletedCell = rowContainingTheOldCell.erase( itrPointingTheOldCell );
+
+    std::vector< std::unique_ptr<SpreadsheetCell> >::iterator itrPointingTheNewCell;
+    try {
+        if ( function == Spreadsheet::Function::sum ) {
+            itrPointingTheNewCell =
+                rowContainingTheOldCell.emplace(
+                    itrFollowingTheDeletedCell,
+                    new SpreadsheetSumCell( newFunctionCellArguments )
+                )
+            ;
+        }
+        //TODO: add more "else if" for new supported functions
+        for (auto itr = oldCellObservers.cbegin(); itr != oldCellObservers.cend(); ++itr) {
+            if ( dynamic_cast<SpreadsheetFunctionCell*>(*itr) != nullptr ) {
+                dynamic_cast<SpreadsheetFunctionCell*>(*itr)->addArgument( (*itrPointingTheNewCell).get() );
+            }
+        }
+    }
+    catch (...) {
+        const auto itrFollowingTheLastDeletedCell =
+            ( rowContainingTheOldCell.size() == numberOfColumns ) ?
+            rowContainingTheOldCell.erase( itrPointingTheNewCell ) :
+            itrFollowingTheDeletedCell
+        ;
+        if ( oldCellIsEmpty ) {
+            rowContainingTheOldCell.emplace(
+                itrFollowingTheLastDeletedCell,
+                new SpreadsheetEmptyCell()
+            );
+        }
+        else if ( ! oldCellIsFunction ) {
+            if ( ! oldCellIsNumeric ) {
+                rowContainingTheOldCell.emplace(
+                    itrFollowingTheLastDeletedCell,
+                    new SpreadsheetRawTextualCell(oldCellAsText)
+                );
+            }
+            else {
+                rowContainingTheOldCell.emplace(
+                    itrFollowingTheLastDeletedCell,
+                    new SpreadsheetRawNumericCell(oldCellAsNumericValue)
+                );
+            }
+        }
+        else {
+            if ( oldCellFunction == Spreadsheet::Function::sum ) {
+                rowContainingTheOldCell.emplace(
+                    itrFollowingTheLastDeletedCell,
+                    new SpreadsheetSumCell(oldCellArguments)
+                );
+            }
+            // TODO: add more "else if" for more functions
+        }
+        for (auto itr = oldCellObservers.cbegin(); itr != oldCellObservers.cend(); ++itr) {
+            if ( dynamic_cast<SpreadsheetFunctionCell*>(*itr) != nullptr ) {
+                dynamic_cast<SpreadsheetFunctionCell*>(*itr)->addArgument( getCellAt(coordinates) );
+            }
+        }
+        throw;
+    }
+    //TODO: Once this class will be a derivative of the "Subject" class, insert the "this->notify()" call here
 }
 
 SpreadsheetCell* Spreadsheet::getCellAt(const SpreadsheetCellCoordinates& coordinates) const {
