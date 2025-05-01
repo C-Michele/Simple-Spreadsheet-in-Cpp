41e501bbc5ba34111f5afa8fb0d8accc84ed2305
Michele Comparini <141649328+C-Michele@users.noreply.github.com>
2025-05-01 18:58:20 +0200

    Added the first scaffolding for GUI

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-// wxWidgets "Hello World" Program
-
 // For compilers that support precompilation, includes "wx/wx.h".
 #include <wx/wxprec.h>
 
@@ -7,65 +5,28 @@
     #include <wx/wx.h>
 #endif
 
-class MyApp : public wxApp {
-    public:
-        virtual bool OnInit();
-};
+#include <memory>
 
-class MyFrame : public wxFrame {
+#include "src/Spreadsheet.h"
+#include "src/SpreadsheetView.h"
+#include "src/SpreadsheetController.h"
+
+class SimpleSpreadsheet : public wxApp {
     public:
-        MyFrame();
+        SimpleSpreadsheet() = default;
+        virtual bool OnInit() override;
     private:
-        void OnHello(wxCommandEvent& event);
-        void OnExit(wxCommandEvent& event);
-        void OnAbout(wxCommandEvent& event);
+        std::unique_ptr<Spreadsheet> spreadsheet;
+        std::unique_ptr<SpreadsheetController> spreadsheetController;
+        std::unique_ptr<SpreadsheetView> spreadsheetView;
 };
 
-enum {
-    ID_Hello = 1
-};
-
-wxIMPLEMENT_APP(MyApp);
-
-bool MyApp::OnInit() {
-    MyFrame *frame = new MyFrame();
-    frame->Show(true);
+bool SimpleSpreadsheet::OnInit() {
+    spreadsheet = std::make_unique<Spreadsheet>(5, 5);
+    spreadsheetController = std::make_unique<SpreadsheetController>(spreadsheet.get());
+    spreadsheetView = std::make_unique<SpreadsheetView>(spreadsheet.get(), spreadsheetController.get());
+    spreadsheetView->Show(true);
     return true;
 }
 
-MyFrame::MyFrame() : wxFrame(NULL, wxID_ANY, "Hello World") {
-    wxMenu *menuFile = new wxMenu;
-    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-                     "Help string shown in status bar for this menu item");
-    menuFile->AppendSeparator();
-    menuFile->Append(wxID_EXIT);
-
-    wxMenu *menuHelp = new wxMenu;
-    menuHelp->Append(wxID_ABOUT);
-
-    wxMenuBar *menuBar = new wxMenuBar;
-    menuBar->Append(menuFile, "&File");
-    menuBar->Append(menuHelp, "&Help");
-
-    SetMenuBar( menuBar );
-
-    CreateStatusBar();
-    SetStatusText("Welcome to wxWidgets!");
-
-    Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
-    Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
-    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
-}
-
-void MyFrame::OnExit(wxCommandEvent& event) {
-    Close(true);
-}
-
-void MyFrame::OnAbout(wxCommandEvent& event) {
-    wxMessageBox("This is a wxWidgets Hello World example",
-                 "About Hello World", wxOK | wxICON_INFORMATION );
-}
-
-void MyFrame::OnHello(wxCommandEvent& event) {
-    wxLogMessage("Hello world from wxWidgets!");
-}
+wxIMPLEMENT_APP(SimpleSpreadsheet);
